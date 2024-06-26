8dbb70cf274a95722bac4e28ab9850ee3fa3f276
Vincent Tse <vncntts@gmail.com>
2024-06-25 23:29:20 -0400

    Add: Structure for FGOEFinder

---
+++ fgoefinder_services/src/main/java/com/fgoefinder_services/fgoefinder_services/FgoefinderServicesApplication.java
@@ -0,0 +1,12 @@
+package com.fgoefinder_services.fgoefinder_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FgoefinderServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FgoefinderServicesApplication.class, args);
+	}
+}
---
+++ fgoefinder_services/src/test/java/com/fgoefinder_services/fgoefinder_services/FgoefinderServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fgoefinder_services.fgoefinder_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FgoefinderServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
