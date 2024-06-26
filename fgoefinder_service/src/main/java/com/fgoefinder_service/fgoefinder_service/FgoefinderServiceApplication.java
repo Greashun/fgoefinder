b4b23d6daaa21101a8d043c41d5087c301b52fae
Vincent Tse <vncntts@gmail.com>
2024-06-26 11:28:46 -0400

    Init: Backend Service Reinit

---
+++ fgoefinder_service/src/main/java/com/fgoefinder_service/fgoefinder_service/FgoefinderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fgoefinder_service.fgoefinder_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FgoefinderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FgoefinderServiceApplication.class, args);
+	}
+
+}
---
+++ fgoefinder_service/src/test/java/com/fgoefinder_service/fgoefinder_service/FgoefinderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fgoefinder_service.fgoefinder_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FgoefinderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
